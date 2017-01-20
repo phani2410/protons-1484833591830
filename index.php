fe7fa3185b99c71c7e81c1ef02de05f47ab7e5bb
PHANINDRA SURA <sura.phanindra@gmail.com>
2017-01-20 11:10:44 +0530

    upd

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
 <?php
 
 if(isset($_GET['q']) && $_GET['q']!='') {
-    require_once dirname(__FILE__).'/config.php';
-    require_once dirname(__FILE__).'/lib/TwitterSentimentAnalysis.php';
+    include_once '/home/vcap/app/htdocs/config.php';
+    include_once '/home/vcap/app/htdocs/lib/TwitterSentimentAnalysis.php';
 
     $TwitterSentimentAnalysis = new TwitterSentimentAnalysis(DATUMBOX_API_KEY,TWITTER_CONSUMER_KEY,TWITTER_CONSUMER_SECRET,TWITTER_ACCESS_KEY,TWITTER_ACCESS_SECRET);
 
